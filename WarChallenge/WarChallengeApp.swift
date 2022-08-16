9136c9a014868b0f715cb68037cdf4eff6be033b
Rehnuma <rehnuma.deepty@therapservices.net>
2022-08-16 15:52:55 +0600

    Initial Commit

---
+++ WarChallenge/WarChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarChallengeApp.swift
+//  WarChallenge
+//
+//  Created by rehnuma.deepty on 16/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WarChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WarChallengeUITests/WarChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WarChallengeUITestsLaunchTests.swift
+//  WarChallengeUITests
+//
+//  Created by rehnuma.deepty on 16/8/22.
+//
+
+import XCTest
+
+class WarChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
